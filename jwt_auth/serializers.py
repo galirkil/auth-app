d36e5ed89e6cbd48a702a75d8c2574633aec71f1
galirkil <galirkil@gmail.com>
2024-03-05 15:50:47 +0300

    Add serializers for drf-spectacular

--- jwt_auth/serializers.py
+++ jwt_auth/serializers.py
@@ -8,3 +8,16 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email']
+
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+
+class DetailCodeErrorSerializer(serializers.Serializer):
+    detail = serializers.CharField()
+    code = serializers.CharField()
+
+
+class DetailErrorSerializer(serializers.Serializer):
+    detail = serializers.CharField()
