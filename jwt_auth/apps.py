b573d85246b4357a5b7c90d15bab1145de0ef0b2
galirkil <galirkil@gmail.com>
2024-03-05 15:23:50 +0300

    Start app jwt_auth

---
+++ jwt_auth/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class JwtAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jwt_auth'
+    verbose_name = 'JWT аутентификация'
